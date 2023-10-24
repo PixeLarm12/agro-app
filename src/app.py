7dc46c87f4dbcc8102968504045f3d14e01f197a
Lucas R. Domingues <lucasrdomingues-lrd@hotmail.com>
2023-10-24 10:57:49 -0300

    Prepared search view to receive information

--- src/app.py
+++ src/app.py
@@ -5,3 +5,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/search")
+def search():
+    cities = [
+        {"id": 1, "name": "Bauru"},
+        {"id": 22, "name": "Araraquara"},
+        {"id": 12, "name": "Jau"},
+    ]
+
+    return render_template("search.html", cities=cities)
