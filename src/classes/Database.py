843439b26645c4fb515129b0c3da4cb7e7f2aef8
PixeLarm12 <lucasrdomingues-lrd@hotmail.com>
2023-11-07 21:17:55 -0300

    Added real data for working

--- src/classes/Database.py
+++ src/classes/Database.py
@@ -10,62 +10,15 @@ def getCultureById(id):
     return None
 
 def citiesByCulture(id):
-    cities = []
+    citiesIds = []
+    data = []
 
     for row in cities_cultures():
         if int(row["culture_id"]) == int(id):
-            cities.append(row)
+            citiesIds.append(row["city_id"])
 
-    return cities
+    for city in cities():
+        if int(city["id"]) in citiesIds:
+            data.append(city)
 
-# def Culturas():
-#      return {
-#         "SP": [
-#             {"idCultura": 41, "cultura": "Cana-de-açúcar", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 42, "cultura": "Café", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 43, "cultura": "Laranja", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 44, "cultura": "Milho", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 45, "cultura": "Soja", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 46, "cultura": "Tomate", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 47, "cultura": "Batata", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 48, "cultura": "Citros", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 49, "cultura": "Algodão", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 50, "cultura": "Feijão", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 51, "cultura": "Hortaliças", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 52, "cultura": "Flores", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 53, "cultura": "Alface", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 54, "cultura": "Maracujá", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 55, "cultura": "Morango", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 56, "cultura": "Banana", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 57, "cultura": "Abóbora", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 58, "cultura": "Mandioca", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 59, "cultura": "Uva", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 60, "cultura": "Manga", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 61, "cultura": "Goiaba", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#             {"idCultura": 62, "cultura": "Maçã", "safra": "2022-2023", "inicioPlantio": "setembro", "ciclo": "90", "regiao": 1},
-#         ],
-#     }
-
-# def Cidades():
-#     return [
-#         {"id": 1, "nome": "São Paulo", "idCultura": 51},
-#         {"id": 1, "nome": "Guarulhos", "idCultura": 52},
-#         {"id": 1, "nome": "Campinas", "idCultura": 42},
-#         {"id": 1, "nome": "São Bernardo do Campo", "idCultura": 50},
-#         {"id": 1, "nome": "Santo André", "idCultura": 53},
-#         {"id": 1, "nome": "Osasco", "idCultura": 54},
-#         {"id": 1, "nome": "São José dos Campos", "idCultura": 55},
-#         {"id": 1, "nome": "Ribeirão Preto", "idCultura": 41},
-#         {"id": 1, "nome": "Sorocaba", "idCultura": 44},
-#         {"id": 1, "nome": "Santos", "idCultura": 56},
-#         {"id": 1, "nome": "Mogi das Cruzes", "idCultura": 57},
-#         {"id": 1, "nome": "Diadema", "idCultura": 58},
-#         {"id": 1, "nome": "Jundiaí", "idCultura": 59},
-#         {"id": 1, "nome": "Piracicaba", "idCultura": 41},
-#         {"id": 1, "nome": "Carapicuíba", "idCultura": 60},
-#         {"id": 1, "nome": "Bauru", "idCultura": 43},
-#         {"id": 1, "nome": "São Vicente", "idCultura": 61},
-#         {"id": 1, "nome": "Itaquaquecetuba", "idCultura": 50},
-#         {"id": 1, "nome": "Franca", "idCultura": 42},
-#         {"id": 1, "nome": "Pindamonhangaba", "idCultura": 62},
-#     ]
+    return data
